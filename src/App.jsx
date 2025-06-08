ef6423ee34bcfb4c557afe2327ff774fb91d4b53
tanyapyndiah <tppyndiah@me.com>
2025-06-08 12:50:59 -0400

    changed stuff

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />}/>
-        </Routes>
-    </BrowserRouter>
+    <MainPage/>
     </>
     
   )
